6e89111e4d9e04ab657250c28a489d49dcb5fae3
gteix <gabriel.silveira@dcomp.ufs.br>
2023-09-27 16:17:50 -0300

    Criando as instâncias do google maps

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ public class Main
     public static void main(String[] args) {
         Grafo grafo1 = new Grafo(9);
 
-        grafo1.adicionarVertice(1,"0");
-        grafo1.adicionarVertice(2,"1");
-        grafo1.adicionarVertice(3,"2");
+        grafo1.adicionarVertice(1,"Aracaju, SE");
+        grafo1.adicionarVertice(2,"Barra dos Coqueiros, SE");
+        grafo1.adicionarVertice(3,"Salvador, BA");
         grafo1.adicionarVertice(4,"3");
         grafo1.adicionarVertice(5,"4");
         grafo1.adicionarVertice(6,"5");
@@ -13,8 +13,8 @@ public class Main
         grafo1.adicionarVertice(8,"7");
         grafo1.adicionarVertice(9,"8");
 
-        grafo1.adicionarAresta(1,2,4);
-        grafo1.adicionarAresta(2,3,8);
+        grafo1.adicionarAresta(1,3,336); // aju - salvador
+        grafo1.adicionarAresta(1,2,9); // aju - barra
         grafo1.adicionarAresta(3,4,7);
         grafo1.adicionarAresta(4,5,9);
         grafo1.adicionarAresta(5,6,10);
