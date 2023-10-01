8e5032f8fcb765e4a8849f16dc2c414b10fc418d
Gabriel Teixeira Silveira <124623217+GabrielTxrs@users.noreply.github.com>
2023-10-01 17:16:00 -0300

    Terminei a instanciacao de cidades para nosso mapa

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.security.interfaces.RSAKey;
 public class Main
 {
     public static void main(String[] args) {
-        Grafo grafo1 = new Grafo(52);
+        Grafo grafo1 = new Grafo(54);
 
         grafo1.adicionarVertice(1, "Aracaju,SE");                   // 1
         grafo1.adicionarVertice(2, "Caninde de Sao Francisco,SE");  // 1
@@ -35,7 +35,7 @@ public class Main
         grafo1.adicionarVertice(28, "Cabo Frio,RJ");                // 14
         grafo1.adicionarVertice(29, "Sao Paulo,SP");                // 15
         grafo1.adicionarVertice(30, "Sao Carlos,SP");               // 15
-        grafo1.adicionarVertice(31, "Curitiva,PR");                 // 16
+        grafo1.adicionarVertice(31, "Curitiba,PR");                 // 16
         grafo1.adicionarVertice(32, "Maringa,PR");                  // 16
         grafo1.adicionarVertice(33, "Chapeco,SC");                  // 17
         grafo1.adicionarVertice(34, "Florianopolis,SC");            // 17
@@ -57,67 +57,70 @@ public class Main
         grafo1.adicionarVertice(50, "Oiapoque,AP");                 // 25
         grafo1.adicionarVertice(51, "Belem,PA");                    // 26
         grafo1.adicionarVertice(52, "Santarem,PA");                 // 26
+        grafo1.adicionarVertice(53, "Brasilia,DF");                 // 27
+        grafo1.adicionarVertice(54, "Vitoria da Conquista,BA");     // 2
 
-
-        grafo1.adicionarAresta(1, 2, 196);  // Aju - Caninde de Sao Francisco
-        grafo1.adicionarAresta(2, 3, 502); // Caninde de Sao Francisco - Salvador
-        grafo1.adicionarAresta(3, 4, 116); // Salvador - Feira
-        grafo1.adicionarAresta(4, 5, 565); // Feira de Santana - Maceio
-        grafo1.adicionarAresta(5, 6, 129); // Maceio - Maragogi
-        grafo1.adicionarAresta(6, 7, 135); // Maragogi - Recife
-        grafo1.adicionarAresta(7, 8, 713); // Recife - Petrolina
-        grafo1.adicionarAresta(8, 9, 664); // Petrolina - Campina Grande
-        grafo1.adicionarAresta(9, 10, 127); // Campina Grande - Joao Pessoa
-        grafo1.adicionarAresta(10, 11, 181); // Joao Pessoa - Natal
-        grafo1.adicionarAresta(11, 12, 282); // Natal - Mossoro
-        grafo1.adicionarAresta(12, 13, 238); // Mossoro - Fortaleza
-        grafo1.adicionarAresta(13, 14, 493); // Fortaliza - Juazeiro do Norte
-        grafo1.adicionarAresta(14, 15, 590); // Juazeiro do Norte - Teresina
-        grafo1.adicionarAresta(15, 16, 338); // Teresina - Parnaiba
-
-        grafo1.adicionarAresta(16, 17, 440); // Parnaiba - Sao Luis
-        grafo1.adicionarAresta(17, 18, 800); // Sao Luis - Balsas
-        grafo1.adicionarAresta(18, 19, 600); // Balsas - Palmas
-        grafo1.adicionarAresta(19, 20, 380); // Palmas - Araguaina
-        grafo1.adicionarAresta(20, 21, 1140); // Araguaina - Goiania
-        grafo1.adicionarAresta(21, 22, 230); // Goiania - Rio Verde
-        grafo1.adicionarAresta(22, 23, 870); // Rio Verde - Belo Horizonte
-        grafo1.adicionarAresta(23, 24, 440); // Belo Horizonte - Pocos de Caldas
-        grafo1.adicionarAresta(24, 25, 900); // Pocos de Caldas - Vitoria
-        grafo1.adicionarAresta(25, 26, 130); // Vitoria - Linhares
-        grafo1.adicionarAresta(26, 27, 340); // Linhares - Rio de Janeiro
-        grafo1.adicionarAresta(27, 28, 150); // Rio de Janeiro - Cabo Frio
-        grafo1.adicionarAresta(28, 29, 570); // Cabo Frio - Sao Paulo
-        grafo1.adicionarAresta(29, 30, 230); // Sao Paulo - Sao Carlos
-        grafo1.adicionarAresta(30, 31, 540); // Sao Carlos - Curitiba
-        grafo1.adicionarAresta(31, 32, 420); // Curitiba - Maringa
-        grafo1.adicionarAresta(32, 33, 610); // Maringa - Chapeco
-        grafo1.adicionarAresta(33, 34, 550); // Chapeco - Florianopolis
-        grafo1.adicionarAresta(34, 35, 460); // Florianopolis - Porto Alegre
-        grafo1.adicionarAresta(35, 36, 260); // Porto Alegre - Pelotas
-        grafo1.adicionarAresta(36, 37, 1500); // Pelotas - Campo Grande
-        grafo1.adicionarAresta(37, 38, 310); // Campo Grande - Ponta Pora
-        grafo1.adicionarAresta(38, 39, 1000); // Ponta Pora - Cuiaba
-        grafo1.adicionarAresta(39, 40, 480); // Cuiaba - Sinop
-        grafo1.adicionarAresta(40, 41, 1400); // Sinop - Porto Velho
-        grafo1.adicionarAresta(41, 42, 480); // Porto Velho - Cacoal
-        grafo1.adicionarAresta(42, 43, 980); // Cacoal - Rio Branco
-        grafo1.adicionarAresta(43, 44, 630); // Rio Branco - Cruzeiro do Sul
-        grafo1.adicionarAresta(44, 45, 2000); // Cruzeiro do Sul - Manaus
-        grafo1.adicionarAresta(45, 46, 270); // Manaus - Itacoatiara
-        grafo1.adicionarAresta(46, 47, 970); // Itacoatiara - Boa Vista
-        grafo1.adicionarAresta(47, 48, 140); // Boa Vista - Caracarai
-        grafo1.adicionarAresta(48, 49, 2410); // Caracarai - Macapa
-        grafo1.adicionarAresta(49, 50, 580); // Macapa - Oiapoque
-        grafo1.adicionarAresta(50, 51, 1100); // Oiapoque - Belem
-        grafo1.adicionarAresta(51, 52, 1230); // Belem - Santarem
-        grafo1.adicionarAresta(52, 1,  2730);  // Santarem - Aracaju
-
-
-        grafo1.imprimirGrafo();
-
-
+        grafo1.adicionarAresta(1, 2, 196);  // Aracaju 1 - Caninde de Sao Francisco 2
+        grafo1.adicionarAresta(1, 3, 320); // Aracaju 1 - Salvador 3
+        grafo1.adicionarAresta(1, 5, 270); // Aracaju 1 - Maceio 5
+        grafo1.adicionarAresta(2, 4, 380); // Caninde de Sao Francisco 2 - Feira de Santana 4
+        grafo1.adicionarAresta(3, 4, 116); // Salvador 3 - Feira de Santana 4
+        grafo1.adicionarAresta(3, 54, 510); // Salvador 3 - Vitoria da Conquista 54
+        grafo1.adicionarAresta(5, 6, 120); // Maceio 5 - Maragogi 6
+        grafo1.adicionarAresta(6, 7, 130); // Maragogi 6 - Recife 7
+        grafo1.adicionarAresta(7, 9, 200); // Recife 7 - Campina Grande 9
+        grafo1.adicionarAresta(2, 8, 381); // Caninde de Sao Francisco 2 - Petrolina 8
+        grafo1.adicionarAresta(9, 10, 127); // Campina Grande 9 - Joao Pessoa 10
+        grafo1.adicionarAresta(10, 11, 181); // Joao Pessoa 10 - Natal 11
+        grafo1.adicionarAresta(11, 12, 282); // Natal 11 - Mossoro 12
+        grafo1.adicionarAresta(12, 13, 238); // Mossoro 12 - Fortaleza 13
+        grafo1.adicionarAresta(13, 14, 493); // Fortaleza 13 - Juazeiro do Norte 14
+        grafo1.adicionarAresta(14, 15, 590); // Juazeiro do Norte 14 - Teresina 15
+        grafo1.adicionarAresta(15, 16, 338); // Teresina 15 - Parnaiba 16
+        grafo1.adicionarAresta(15, 18, 570); // Teresina 15 - Balsas 18
+        grafo1.adicionarAresta(16, 17, 440); // Parnaiba 16 - Sao Luis 17
+        grafo1.adicionarAresta(17, 15, 430); // Sao Luis 17 - Teresina 15
+        grafo1.adicionarAresta(18, 20, 280); // Balsas 18 - Araguaina 20
+        grafo1.adicionarAresta(19, 20, 380); // Palmas 19 - Araguaina 20
+        grafo1.adicionarAresta(19, 21, 820); // Palmas 19 - Goiania 21
+        grafo1.adicionarAresta(21, 22, 230); // Goiania 21 - Rio Verde 22
+        grafo1.adicionarAresta(23, 25, 510); // Belo Horizonte 23 - Vitoria 25
+        grafo1.adicionarAresta(23, 54, 830); // Belo Horizonte 23 - Vitoria da Conquista 54
+        grafo1.adicionarAresta(22, 37, 610); // Rio Verde 22 - Campo Grande 37
+        grafo1.adicionarAresta(23, 24, 440); // Belo Horizonte 23 - Pocos de Caldas 24
+        grafo1.adicionarAresta(25, 26, 130); // Vitoria 25 - Linhares 26
+        grafo1.adicionarAresta(26, 27, 340); // Linhares 26 - Rio de Janeiro 27
+        grafo1.adicionarAresta(27, 28, 150); // Rio de Janeiro 27 - Cabo Frio 28
+        grafo1.adicionarAresta(28, 29, 570); // Cabo Frio 28 - Sao Paulo 29
+        grafo1.adicionarAresta(29, 30, 230); // Sao Paulo 29 - Sao Carlos 30
+        grafo1.adicionarAresta(30, 31, 540); // Sao Carlos 30 - Curitiba 31
+        grafo1.adicionarAresta(31, 32, 420); // Curitiba 31 - Maringa 32
+        grafo1.adicionarAresta(32, 33, 610); // Maringa 32 - Chapeco 33
+        grafo1.adicionarAresta(33, 34, 550); // Chapeco 33 - Florianopolis 34
+        grafo1.adicionarAresta(34, 35, 460); // Florianopolis 34 - Porto Alegre 35
+        grafo1.adicionarAresta(35, 36, 260); // Porto Alegre 35 - Pelotas 36
+        grafo1.adicionarAresta(32, 37, 550); // Maringa 32 - Campo Grande 37
+        grafo1.adicionarAresta(37, 38, 310); // Campo Grande 37 - Ponta Pora 38
+        grafo1.adicionarAresta(37, 39, 700); // Campo Grande 37  - Cuiaba 39
+        grafo1.adicionarAresta(39, 40, 480); // Cuiaba 39 - Sinop 40
+        grafo1.adicionarAresta(40, 42, 930); // Sinop 40 - Cacoal 42
+        grafo1.adicionarAresta(41, 42, 480); // Porto Velho 41 - Cacoal 42
+        grafo1.adicionarAresta(41, 43, 510); // Cacoal 42 - Rio Branco 43
+        grafo1.adicionarAresta(43, 44, 630); // Rio Branco 43 - Cruzeiro do Sul 44
+        grafo1.adicionarAresta(41, 45, 880); // Porto Velho 41 - Manaus 45
+        grafo1.adicionarAresta(45, 46, 270); // Manaus 45 - Itacoatiara 46
+        grafo1.adicionarAresta(40, 52, 1290); // Sinop 40 - Santarem 52
+        grafo1.adicionarAresta(45, 48, 640); // Manaus 45 - Caracarai 48
+        grafo1.adicionarAresta(48, 47, 140); // Boa Vista 47 - Caracarai 48
+        grafo1.adicionarAresta(51, 49, 520); // Belem 51 - Macapa 49
+        grafo1.adicionarAresta(51, 17, 570); // Belem 51 - Sao Luis 17
+        grafo1.adicionarAresta(49, 50, 570); // Macapa 49 - Oiapoque 50
+        grafo1.adicionarAresta(20, 52, 1180); // Araguaina 20 - Santarem 52
+        grafo1.adicionarAresta(21, 53,  200);  // Goiania 21 - Brasilia 53
+        grafo1.adicionarAresta(22, 39,  700);  // Rio Verde 22 - Cuiaba 39
+        grafo1.adicionarAresta(4, 53, 1320); // Feira de Santana 4 - Brasilia 53
         grafo1.dijkstra(grafo1.criarMatrizDePesos(), 0);
+        grafo1.imprimirGrafo();
 
 
     }
